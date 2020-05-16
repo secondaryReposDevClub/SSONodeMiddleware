1232bd502a5ebd73e2942147912958fd66161048
Jatin Prakash <jatinprakash1511@gmail.com>
2020-05-16 12:37:24 +0530

    client fixed

--- src/server.js
+++ src/server.js
@@ -1,46 +1,44 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const port = 5000;
 const app = express();
 
+app.use(cors());
+app.use(cookieParser());
+
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
-const extractToken = (req, res, next) => {
-    const { token } = req.query;
-    req.token = token;
-    next();
-}
 
-app.use(extractToken);
-
+// this the middleware every client will need and thats it.
 const auth = async (req, res, next) => {
-    const token = req.token || req.header('x-auth-token'); // or we can also use this, req.token || req.header('x-auth-token'); depends on the client
-
-    console.log(req.header);
+    const { token } = req.cookies;
 
     if (!token) {
         // then we should redirect to the SSO server
         const redirectURL = `${req.protocol}://${req.headers.host}${req.path}`;
-
         return res.redirect('http://localhost:3000/user/login?serviceURL=' + redirectURL);
     }
 
     const config = {
         headers: {
-            'x-auth-token': token
+            'auth-token': token
         }
     }
 
     try {
-        const body = '';
-        // else we have the token, so verify it
-        const { data } = await axios.post('http://localhost:3000/auth', body, config);
+        // we have the token, so verify it
+        const body = '' // empty body as we don't need to send anything
+
+        const { data } = await axios.post('http://localhost:3000/auth', body, config); // leave an empty body
         req.user = data.user
         next();
     }
     catch (err) {
+        res.clearCookie('token');
         next(err);
     }
 }
@@ -52,17 +50,10 @@ app.get('/', auth, (req, res) => {
     return res.render('index', { user })
 });
 
+app.get('/home',(req, res) => {
+    res.send('Home page');
+});
+
 app.listen(port, () => {
     console.log(`Test server up and running on port:${port}`);
 });
-
-app.get('/yo', (req, res) => {
-    req.set('user', 'yoyoyo')
-    return res.redirect('/new');
-})
-
-app.get('/new', (req, res) => {
-    console.log(req.headers)
-    console.log(req.header('user'))
-    res.send('new page');
-})
